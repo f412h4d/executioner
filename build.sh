2e6cc1439c37eb59fe055b89795de2f8b3424361
Amir <amir.f@capsmark.co.uk>
2024-08-13 16:31:35 +0330

    feat: updated the build.sh file

--- build.sh
+++ build.sh
@@ -3,18 +3,15 @@
 # Check if the --prod flag is provided
 if [ "$1" == "--prod" ]; then
     echo "Using production configuration..."
-    cp CMakeLists.prod.txt CMakeLists.txt
+    cp -rf CMakeLists.prod.txt CMakeLists.txt
     cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=/home/amir_f/vcpkg/scripts/buildsystems/vcpkg.cmake
     cmake --build build -- -j4
 else
     echo "Using local configuration..."
-    cp CMakeLists.local.txt CMakeLists.txt
+    cp -rf CMakeLists.local.txt CMakeLists.txt
     cmake -B build -S .
     cmake --build build -- -j8
 fi
-
-# Clean up by removing the temporary CMakeLists.txt
-rm CMakeLists.txt
 
 make
 
