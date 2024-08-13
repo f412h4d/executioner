dbdd8f2e8612ab915498cfc701514685f7595dcd
Amir <amir.f@capsmark.co.uk>
2024-08-13 15:46:41 +0330

    feat: updated the build.sh to have make as well

--- build.sh
+++ build.sh
@@ -5,10 +5,12 @@ if [ "$1" == "--prod" ]; then
     echo "Using production configuration..."
     cp CMakeLists.prod.txt CMakeLists.txt
     cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=/home/amir_f/vcpkg/scripts/buildsystems/vcpkg.cmake
+    cmake --build build
 else
     echo "Using local configuration..."
     cp CMakeLists.local.txt CMakeLists.txt
     cmake -B build -S .
+    cmake --build build -- -j8
 fi
 
 # Clean up by removing the temporary CMakeLists.txt
