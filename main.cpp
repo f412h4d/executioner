309b20be3a41ef368e5a74a2188ec34b54c55c58
Amir <amir.f@capsmark.co.uk>
2024-08-20 17:23:20 +0330

    .

--- main.cpp
+++ main.cpp
@@ -75,8 +75,10 @@ void pubsub_callback(pubsub::Message const &message, pubsub::AckHandler ack_hand
 
       auto balance = Margin::getBalance(apiParams, "USDT");
       signal_logger->warn("Balance: {}", balance);
-      double quantity = std::floor((balance * LEVERAGE / price_settings->calculated_price) * 100) / 100;
-      double price = std::floor(price_settings->calculated_price * 100) / 100;
+      signal_logger->warn("Price: {}", price_settings->calculated_price);
+      double quantity = std::floor((balance * LEVERAGE / price_settings->calculated_price) * 1000) / 1000;
+      double price = std::floor(price_settings->calculated_price * 1000) / 1000;
+      signal_logger->warn("Price FLOOR: {}", price);
       signal_logger->warn("Quantity: {}", quantity);
 
       SignalService::process(signal_settings->signal.value, apiParams, quantity, price);
